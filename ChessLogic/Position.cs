d48e3e791e951d59d71fd58866683c0da4efa62b
LexLuthor123000 <vasicpavle47@gmail.com>
2025-03-04 21:02:43 +0100

    Add project files.

---
+++ ChessLogic/Position.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    internal class Position
+    {
+        public int Row {  get; } // y osa
+        public int Column { get; } // x osa
+
+        // U tutorialu koji ja gledam on predpostavlja da 
+    }
+}
